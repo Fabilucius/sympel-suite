8880ffa0cadb7164500548100f58fe36e0e67e15
Fabian B <49801250+Fabilucius@users.noreply.github.com>
2022-02-09 15:50:47 +0100

    The database now checks if its still connected to its remote instance and will try a reconnect if it somehow lost connection

--- sympel-database/src/main/java/de/fabilucius/sympel/database/AbstractDatabase.java
+++ sympel-database/src/main/java/de/fabilucius/sympel/database/AbstractDatabase.java
@@ -16,11 +16,19 @@ public class AbstractDatabase implements Database {
 
     private static final Logger LOGGER = Bukkit.getLogger();
 
-    private final Connection connection;
+    private Connection connection;
+    private final String connectionUrl;
+    private final Credentials credentials;
 
     protected AbstractDatabase(String connectionUrl, Credentials credentials) {
         Preconditions.checkNotNull(connectionUrl, "connectionUrl cannot be null");
         Preconditions.checkNotNull(credentials, "credentials cannot be null");
+        this.connectionUrl = connectionUrl;
+        this.credentials = credentials;
+        this.connectToDatabase();
+    }
+
+    private void connectToDatabase() {
         try {
             if (credentials.isAuthEmpty()) {
                 this.connection = DriverManager.getConnection(connectionUrl);
@@ -37,7 +45,7 @@ public class AbstractDatabase implements Database {
     @Override
     public void customUpdate(String query) {
         Preconditions.checkState(!Strings.isNullOrEmpty(query), "query cannot be empty or null");
-
+        this.ensureConnectionIntegrity();
         try {
             PreparedStatement preparedStatement = this.getConnection().prepareStatement(query);
             preparedStatement.executeUpdate();
@@ -50,7 +58,7 @@ public class AbstractDatabase implements Database {
     @Override
     public final ResultSet customQuery(String query) {
         Preconditions.checkState(!Strings.isNullOrEmpty(query), "query cannot be empty or null");
-
+        this.ensureConnectionIntegrity();
         try {
             PreparedStatement preparedStatement = this.getConnection().prepareStatement(query);
             return preparedStatement.executeQuery();
@@ -67,7 +75,7 @@ public class AbstractDatabase implements Database {
         Preconditions.checkState(!Strings.isNullOrEmpty(setLogic), "setLogic cannot be null or empty");
         Preconditions.checkState(!Strings.isNullOrEmpty(whereLogic), "whereLogic cannot be null or empty");
         Preconditions.checkState(this.isConnected(), "database doesn't seem to be connected");
-
+        this.ensureConnectionIntegrity();
         String updateQuery = "UPDATE " + table + " SET " + setLogic + " WHERE " + whereLogic;
         try {
             PreparedStatement preparedStatement = this.getConnection().prepareStatement(updateQuery);
@@ -84,7 +92,7 @@ public class AbstractDatabase implements Database {
         Preconditions.checkNotNull(columnsToInsert, "columnsToInsert cannot be null");
         Preconditions.checkNotNull(valuesToInsert, "valuesToInsert cannot be null");
         Preconditions.checkState(this.isConnected(), "database doesn't seem to be connected");
-
+        this.ensureConnectionIntegrity();
         String columns = String.join(",", columnsToInsert);
         String values = valuesToInsert.stream().map(value -> "'" + value + "'").collect(Collectors.joining(","));
         String insertQuery = "INSERT INTO " + table + " (" + columns + ") VALUES (" + values + ")";
@@ -102,7 +110,7 @@ public class AbstractDatabase implements Database {
         Preconditions.checkState(!Strings.isNullOrEmpty(table), "table cannot be null or empty");
         Preconditions.checkNotNull(columnsToSelect, "columnsToSelect cannot be null");
         Preconditions.checkNotNull(whereLogic, "whereLogic cannot be null");
-
+        this.ensureConnectionIntegrity();
         String columns = String.join(",", columnsToSelect);
         String selectQuery = "SELECT " + columns + " FROM " + table + (whereLogic.isEmpty() ? "" : " WHERE " + whereLogic);
         try {
@@ -131,7 +139,7 @@ public class AbstractDatabase implements Database {
     public final boolean valueExistQuery(String table, String whereLogic) {
         Preconditions.checkState(!Strings.isNullOrEmpty(table), "table cannot be null or empty");
         Preconditions.checkNotNull(whereLogic, "whereLogic cannot be null");
-
+        this.ensureConnectionIntegrity();
         ResultSet resultSet = this.selectQuery(table, Lists.newArrayList("*"), whereLogic);
         try {
             return resultSet != null && resultSet.next();
@@ -142,6 +150,7 @@ public class AbstractDatabase implements Database {
 
     @Override
     public final void insertOrUpdateQuery(String table, List<String> columnsToInsert, List<String> valuesToInsert, String whereLogic, String updateLogic) {
+        this.ensureConnectionIntegrity();
         if (this.valueExistQuery(table, "")) {
             this.updateQuery(table, updateLogic, whereLogic);
         } else {
@@ -158,7 +167,22 @@ public class AbstractDatabase implements Database {
         }
     }
 
+    protected final void ensureConnectionIntegrity() {
+        if (!this.isConnected()) {
+            LOGGER.log(Level.INFO, "The connection to the database was interrupted, trying to reestablish connection...");
+            this.connectToDatabase();
+        }
+    }
+
     /* the getter and setter of the class */
+
+    private String getConnectionUrl() {
+        return connectionUrl;
+    }
+
+    private Credentials getCredentials() {
+        return credentials;
+    }
 
     public Connection getConnection() {
         return connection;
